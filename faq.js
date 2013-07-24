83c16f0c299d89d949f83c9a2c1e49eaf7e1c04f
rektide <rektide@voodoowarez.com>
2013-07-24 09:04:41 -0400

    FAQ

---
+++ faq.js
@@ -0,0 +1,41 @@
+var common = require('../common')
+var assert = require('assert')
+var exec = require('child_process').exec
+var tls = require('tls')
+var fs = require('fs')
+
+if(process.platform === 'win32'){
+	console.log("Skipping test, you probably don't have openssl installed.")
+	process.exit()
+}
+
+var options= {
+  key: fs.readFileSync(common.fixturesDir+'/keys/agent2-key.pem'),
+  cert: fs.readFileSync(common.fixturesDir+'/keys/agent2-cert.pem'),
+  ciphers: 'ECDHE-RSA-RC4-SHA'
+}
+
+var reply = 'I AM THE WALRUS', // something recognizable
+	nconns = 0
+	response = ''
+
+process.on('exit', function(){
+	assert.equal(nconns, 1)
+	assert.notEqual(response.indexOf(reply), -1)
+})
+
+var server = tls.createServer(options, function(conn){
+	conn.end(reply)
+	nconns++
+})
+
+server.listen(common.PORT, '127.0.0.1', function(){
+	var cmd = 'openssl s_client -cipher ' + options.ciphers +
+	          ' -connect 127.0.0.1:' + common.PORT
+
+	exec(cmd, function(err, stdout, stderr){
+		if (err) throw err
+		response = stdout
+		server.close()
+	})
+})
